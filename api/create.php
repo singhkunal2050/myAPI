6f3beee51637fc5f8e6b1eb33698db59603e000a
singhkunal2050 <singhkunal2051@gmail.com>
2020-05-28 17:56:47 +0530

    working on create

---
+++ api/create.php
@@ -0,0 +1,41 @@
+<?php 
+  // Headers
+  header('Access-Control-Allow-Origin: *');
+  header('Content-Type: application/json');
+  header('Access-Control-Allow-Methods: POST');
+  header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods, Authorization, X-Requested-With');
+
+  include_once '../config/Database.php';
+  include_once '../models/User.php';
+
+  // Instantiate DB & connect
+  $database = new Database();
+  $db = $database->connect();
+
+  // Instantiate blog post object
+  $user = new User($db);
+
+  // Get raw posted data
+  $data = json_decode(file_get_contents("php://input"));
+
+  
+  // $user->id= $data->id;                // id is auto generated
+  $user->uname = $data->uname;
+  $user->village = $data->village;
+  $user->taluka = $data->taluka;
+  $user->district = $data->district;
+  $user->phoneno = $data->phoneno;
+  $user->adhaarno = $data->adhaarno;
+
+
+  // Create post
+  if($user->create()) {
+    echo json_encode(
+      array('message' => 'Post Created')
+    );
+  } else {
+    echo json_encode(
+      array('message' => 'Post Not Created')
+    );
+  }
+
